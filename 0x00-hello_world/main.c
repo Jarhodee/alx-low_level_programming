1ffd11f4292a9eae10442c57cf089265d0313f0b
Jarhodee <christopherdezmond@gmail.com>
2022-09-08 18:54:42 -0700

    4-puts.c

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -9,4 +9,18 @@ int main(void)
 	return(0);
 }
 
+export CFILE=main.c
+ ./0-preprocessor 
+tail c
+# 942 "/usr/include/stdio.h" 3 4
+
+# 2 "main.c" 2
+
+
+# 3 "main.c"
+
+int main(void)
+{
+	 return (0);
+}
 
