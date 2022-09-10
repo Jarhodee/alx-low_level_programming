ff26a7235a17fb1081283cc9bb4f295091518a8c
Jarhodee <christopherdezmond@gmail.com>
2022-09-09 22:37:44 -0700

    C language

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <stdio.h>
-/**
+ 
+ /**
  * main - Entry point
  * Description: prints lowercase alphabets in reverse
  * Return: Always 0 (success)
@@ -14,5 +15,5 @@ int main(void)
 		putchar(a);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
