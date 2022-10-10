6000d3f7d6f15b50e87b0adc880ac3011cfb6494
Jarho <christopherdezmond@gmail.com>
2022-10-10 15:42:11 -0700

    C

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -27,7 +27,7 @@ int _strlen(char *s)
  * to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
  * @src: string to be copied
- * 
+ *
  * Return: the pointer to dest
  */
 char *_strcpy(char *dest, char *src)
@@ -69,7 +69,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	
+
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
 	{
